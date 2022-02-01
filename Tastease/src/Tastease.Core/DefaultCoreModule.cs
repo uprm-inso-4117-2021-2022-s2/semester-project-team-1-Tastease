793d95f03719feee557ff7fdea1d74b184065c05
kelminator <kemuel.torres1@upr.edu>
2022-02-01 14:54:26 -0400

    Initial Commit

---
+++ Tastease/src/Tastease.Core/DefaultCoreModule.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using Tastease.Core.Interfaces;
+using Tastease.Core.Services;
+
+namespace Tastease.Core;
+
+public class DefaultCoreModule : Module
+{
+  protected override void Load(ContainerBuilder builder)
+  {
+    builder.RegisterType<ToDoItemSearchService>()
+        .As<IToDoItemSearchService>().InstancePerLifetimeScope();
+  }
+}
